50d048bfa8092694ea8fdda5e8bb929ffcba4e1e
horodev <tim.nelke@outlook.com>
2018-12-06 20:33:53 +0100

    Added ServiceWorker

---
+++ scripts/serviceworker.js
@@ -0,0 +1,66 @@
+var CACHE_NAME = "my-cache-v1";
+var cacheUrls = [
+    "/",
+    "/styles/main.css",
+    "/assets/icons/icon512.png",
+    "/assets/icons/icon192.png",
+];
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log("Opened Cache");
+                return cache.addAll(cacheUrls);
+            }
+        )
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+/** Advanced Caching
+ *  ----------------
+ *  If we want that the data a user fetches automatically is cached
+ * We can do something like this:
+ * 
+ *  self.addEventListener("fetch", function(event) {
+ *      event.respondWith(
+ *          caches.match(event.request)
+ *              .then(function(response) {
+ *                  if (response) {
+ *                      return response;
+ *                  }
+ *                  
+ *                  var fetchRequest = event.request.clone();
+ *                  return fetch(fetchRequest).then(
+ *                      function(response) {
+ *                          if (!response || response.status !== 200 || response.type !== "basic")
+ *                              return response;
+ * 
+ *                          var responseToCache = response.clone();
+ *                          caches.open(CACHE_NAME)
+ *                              .then(function(cache) {
+ *                                  cache.put(event.request, responseToCache);
+ *                              });
+ *
+ *                          return response;
+ *                      }
+ *                  )
+ *              }
+ *          )
+ *      );
+ *  });
+ * 
+ */
